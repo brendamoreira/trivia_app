23fbfa31ece69daac5cbc5bfc40c9184a70bee9c
Brenda Moreira <brendanatalia94@hotmail.com>
2021-01-28 16:18:24 +0100

    Add search question test

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -101,6 +101,14 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(data['success'], False)
         self.assertEqual(data['message'], 'method not allowed')
 
+    def test_search_question(self):
+        res = self.client().post('/questions/search', json={'searchTerm': ''})
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+
+
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
