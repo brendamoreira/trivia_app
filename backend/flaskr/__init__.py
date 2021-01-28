845cd74c67096736f0f83469981ab34db280de31
Brenda Moreira <brendanatalia94@hotmail.com>
2021-01-28 18:06:14 +0100

    Change error handler

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -248,13 +248,13 @@ def create_app(test_config=None):
           'message': 'unprocessable'
       }), 422
 
-  @app.errorhandler(400)
+  @app.errorhandler(500)
   def bad_request(error):
       return jsonify({
           'success':False,
-          'error': 400,
-          'message': 'bad request'
-      }), 400
+          'error': 500,
+          'message': 'internal server error'
+      }), 500
 
   @app.errorhandler(405)
   def not_allowed(error):
